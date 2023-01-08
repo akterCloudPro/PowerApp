511b5e2c6ae972b6fb9dd6a2f237c72d2cb1e383
akterCloudPro <73134659+akterCloudPro@users.noreply.github.com>
2023-01-08 16:38:54 +0600

    Create code-blocks.c
    First commit

---
+++ code-blocks.c
@@ -0,0 +1,62 @@
+// Reset Form, Set New Form, Navigate to another screen (Screen5) with Transition 
+ResetForm(Form3);
+NewForm(Form3);
+Navigate(Screen5, ScreenTransition.Fade);
+
+// Date Difference
+DateDiff(DataCardValue4.SelectedDate,DataCardValue5.SelectedDate)
+
+// Date Difference (Conditional)
+If(
+    And(
+        IsBlank(DataCardValue4),
+        IsBlank(DataCardValue5)
+    ),
+    0, 
+    DateDiff(
+        DataCardValue4.SelectedDate,
+        DataCardValue5.SelectedDate
+    ) + 1
+)
+
+// Data Field Visibility  (Conditional field data) 
+If(
+    DataCardValue8.Selected.Value = "Medical Leave",
+    true,
+    false
+)
+
+// Height (Conditional based on Attachment Section's visibility)
+If(
+    Attachments_DataCard1.Visible,
+    671 + Attachments_DataCard1.Height,
+    671
+)
+
+// Filed Visibility (Conditional Form mode)
+If(
+    Form3.Mode = FormMode.New,
+    false,
+    true
+)
+
+
+// (Collection) On Change - Department to relavent Approvers dropdown
+Clear(ApproverManager);
+Reset(DataCardValue3);
+ClearCollect(
+    FilteredApprovers,
+    Filter(
+        Approver,
+        Title = DataCardValue2.Selected.Value
+    )
+);
+ForAll(FilteredApprovers, Collect(ApproverManager, {
+    Value: Approver.DisplayName,
+    DisplayName: Approver.DisplayName,
+    Email: Approver.Email,
+    Picture: Approver.Picture,
+    Claims: Approver.Claims,
+    Department: Approver.Department,
+    JobTitle: Approver.JobTitle
+}))
